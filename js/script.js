5de6b217c55c0a793b75decf34d2cfd1cb2677b3
Vasua Pupkin <kulenko.jhenia@gmail.com>
2022-01-09 00:10:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,218 @@
+  $('.btn__wrapper-1').click(function() {
+  window.setTimeout(function() {
+    $('.btn__wrapper-1').toggleClass('inactive');
+  }, 1000);
+});
+  
+  $('.icon_calendar').click(function() {
+    $('.calendar2').toggleClass('block');
+  
+});
+   $('.btn__wrapper-2').click(function() {
+  window.setTimeout(function() {
+    $('.btn__wrapper-2').toggleClass('inactive');
+  }, 1000);
+});
+
+ $('.btn__wrapper-3').click(function() {
+  window.setTimeout(function() {
+    $('.btn__wrapper-3').toggleClass('inactive');
+  }, 1000);
+});
+
+ $('.btn__wrapper-4').click(function() {
+  window.setTimeout(function() {
+    $('.btn__wrapper-4').toggleClass('inactive');
+  }, 1000);
+});   
+var add_class = document.getElementById('header_nav');
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 10) {
+        add_class.classList.add("fixed");
+    } else {
+        add_class.classList.remove("fixed");
+    }
+});
+var change = document.getElementById('body');
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 2000) {
+        change.classList.add("shadow");
+    } else {
+        change.classList.remove("shadow");
+    }
+});
+function Calendar2(id, year, month) {
+var Dlast = new Date(year,month+1,0).getDate(),
+    D = new Date(year,month,Dlast),
+    DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
+    DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
+    calendar = '<tr>',
+    month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+if (DNfirst != 0) {
+  for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+}else{
+  for(var  i = 0; i < 6; i++) calendar += '<td>';
+}
+for(var  i = 1; i <= Dlast; i++) {
+  if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+    calendar += '<td class="today">' + i;
+  }else{
+    calendar += '<td>' + i;
+  }
+  if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
+    calendar += '<tr>';
+  }
+}
+for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+document.querySelector('#'+id+' tbody').innerHTML = calendar;
+document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
+if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+    document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+}
+}
+Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
+}
+// переключатель плюс месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
+}
+var el = document.getElementById('input_change');
+ var d = new Date();
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+
+  var output = (day<10 ? '0' : '') + day + "-" 
+              + (month<10 ? '0' : '') + month + '-'
+              + d.getFullYear();
+  el.innerHTML = output;
+  jQuery (function ($) {  
+  $(function() {
+    function maskPhone() {
+      var country = $('#country option:selected').val();
+      switch (country) {
+        case "ru":
+          $("#phone").mask("+7(999) 999-99-99");
+          break;
+        case "ua":
+          $("#phone").mask("+380(999) 999-99-99");
+          break;
+        case "by":
+          $("#phone").mask("+375(999) 999-99-99");
+          break;          
+      }    
+    }
+    maskPhone();
+    $('#country').change(function() {
+      maskPhone();
+    });
+  });
+});
+
+// Получаем нужный элемент
+var element = document.querySelector('#target');
+var element2 = document.querySelector('#target2');
+var element3 = document.querySelector('#target3');
+var element4 = document.querySelector('#target4');
+var element5 = document.querySelector('#target5');
+var element6 = document.querySelector('#target6');
+var element7 = document.querySelector('#target7');
+
+var Visible = function (target) {
+  // Все позиции элемента
+  var targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      left: window.pageXOffset + target.getBoundingClientRect().left,
+      right: window.pageXOffset + target.getBoundingClientRect().right,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+    // Получаем позиции окна
+    windowPosition = {
+      top: window.pageYOffset-650,
+      left: window.pageXOffset,
+      right: window.pageXOffset + document.documentElement.clientWidth,
+      bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+200 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element.classList.add('new');
+
+  } else {
+    // Если элемент не видно, то запускаем этот код
+    element.classList.remove('new');
+   
+  }if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+300 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element2.classList.add('new');
+
+  }else{
+element2.classList.remove('new');
+  }if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+400 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element3.classList.add('new');
+
+  }else{
+element3.classList.remove('new');
+  }
+  if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+500 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element4.classList.add('new');
+
+  }else{
+element4.classList.remove('new');
+  }if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+600 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element5.classList.add('new');
+
+  }else{
+element5.classList.remove('new');
+  }if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+750 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element6.classList.add('new');
+
+  }else{
+element6.classList.remove('new');
+  }if(targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top+850 < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+   element7.classList.add('new');
+
+  }else{
+element7.classList.remove('new');
+  };
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function() {
+  Visible (element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible (element);
+
+
